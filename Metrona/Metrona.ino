72931900ce9dd9fc8ea0bcd16897694f8e448a8a
ploopydopsy <william.adebiyi@gmail.com>
2024-12-16 01:01:51 -0700

    Pins Assigned

--- Metrona/Metrona.ino
+++ Metrona/Metrona.ino
@@ -1,9 +1,29 @@
+#include "Arduino.h"
+#include "DFRobotDFPlayerMini.h"
+#include <SoftwareSerial.h>
+#include <LiquidCrystal_I2C.h>
+
+int ResetPin = 9;
+int StartStopPin = 10;
+
+int TimeSignaturePin = 5;
+int BPMButtonPin = 4;
+int BPMPinA = 2;
+int BPMPinB = 3;
+
+int VolumePin = A0;
+
+int BuzzerPin = 6;
+int LedPin = 13;
+
+LiquidCrystal_I2C lcd( 0x27, 16, 2);
+SoftwareSerial Speaker(11,12);
+DFRobotDFPlayerMini DFPlayer;
+
 void setup() {
-  // put your setup code here, to run once:
-test 
+
 }   
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
