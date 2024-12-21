3c8f865d99c5885eb138df870086f2084874512a
ploopydopsy <william.adebiyi@gmail.com>
2024-12-21 03:01:19 -0700

    updare pins
    scrapped volume knob for now, its unnecessary

--- Metrona/Metrona.ino
+++ Metrona/Metrona.ino
@@ -7,11 +7,8 @@ int TimeSignaturePin = 7;
 int BPMButtonPin = 6;
 int BPMPinA = 2;
 int BPMPinB = 3;
-int VolumePin = A0;
 int BuzzerPin = 9;
-
 int LedPins[] = {8, 10, 11, 12, 13};
-
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
