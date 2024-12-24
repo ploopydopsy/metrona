06813912137ad18d0a3606db7c5b19ca876a21d8
ploopydopsy <william.adebiyi@gmail.com>
2024-12-23 17:38:12 -0700

    started encoder and LED
    needs to be finished

--- Metrona/Metrona.ino
+++ Metrona/Metrona.ino
@@ -4,9 +4,9 @@
 int ResetPin = 4;
 int StartStopPin = 5;
 int TimeSignaturePin = 7;
-int BPMButtonPin = 6;
-int BPMPinA = 2;
-int BPMPinB = 3;
+int EButtonPin = 6;
+int EncoderPinA = 2;
+int EncoderPinB = 3;
 int BuzzerPin = 9;
 int LedPins[] = {13, 12, 8, 10, 11};
 LiquidCrystal_I2C lcd(0x27, 16, 2);
@@ -18,19 +18,46 @@ int ledIndex = 0;
 int numLeds = sizeof(LedPins) / sizeof(LedPins[0]);
 int buzzerLength = 1;
 
+volatile int bpm = 100;
+int lastEncoderStateA = LOW;
 
 void setup() {
+  //encoder
+  pinMode(EncoderPinA, INPUT_PULLUP);
+  pinMode(EncoderPinB, INPUT_PULLUP);
+  pinMode(EButtonPin, INPUT_PULLUP);
+
+  //set all leds as outputs
   pinMode(13, OUTPUT);
   pinMode(12, OUTPUT);
   pinMode(8, OUTPUT);
   pinMode(10, OUTPUT);
-  pinMode(9, OUTPUT);
   pinMode(11, OUTPUT);
+  lcd.begin(16,2);
 }   
 
 void loop() {
-  newBPM (0,3);
+
+  ////FIX ME 
+  
+  int currentStateA = digitalRead(EncoderPinA);
+  if (currentStateA != lastEncoderStateA && currentStateA == HIGH) {
+    if (digitalRead(EncoderPinB) != currentStateA) {
+      bpm++; 
+    } else {
+      bpm--; 
+    }
+  bpm = constrain(bpm, 40, 4000);
+  lcd.setCursor(0, 1);
+  lcd.backlight();
+  lcd.print("BPM: ");
+  lcd.print(bpm);
+  lcd.print("   "); 
+  }
+  lastEncoderStateA = currentStateA;
+  newBPM(bpm, 4); 
 }
+
 
 void newBPM (int bpm, int timesig) {
   int buzzerDelay = 60000 / bpm ;
